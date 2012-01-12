ddebca52fa077da3843798cfdcdb016940d9ff74
zhanju <zhanju@9987ea41-4527-47ae-bde9-3645be3cca0f>
2012-01-12 14:12:17 +0000

    migrated into new repos
    git-svn-id: svn://jmedu.wassecurity.net/BrightEdu/trunk@2 9987ea41-4527-47ae-bde9-3645be3cca0f

---
+++ src/com/brightedu/util/ConnectionManager.java
@@ -0,0 +1,170 @@
+package com.brightedu.util;
+
+import java.beans.PropertyVetoException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class ConnectionManager {
+
+	private static Map<String, DataSource> dataSources = new HashMap<String, DataSource>();
+	private static String defaultSource;
+
+	public static void load() {
+		Iterator<DataSource> it = dataSources.values().iterator();
+		while (it.hasNext()) {
+			DataSource source = it.next();
+			if (source instanceof ComboPooledDataSource) {
+				((ComboPooledDataSource) source).close();
+			}
+		}
+		defaultSource = null;
+		Document config = Utils.getXMLDocument(ConnectionManager.class
+				.getResource("/resources/datasource.xml"));
+		NodeList sourceList = config.getElementsByTagName("pool");
+		for (int i = 0; i < sourceList.getLength(); i++) {
+			Element pool = (Element) sourceList.item(i);
+			String poolType = pool.getAttribute("type");
+			String name = pool.getAttribute("name");
+			boolean defaultPool = "true".equals(pool.getAttribute("default")
+					.toLowerCase());
+			if (defaultPool)
+				defaultSource = name;
+			if (dataSources.containsKey("name")) {
+				Log.e("duplicated datasource defined:" + name);
+				break;
+			}
+			if (poolType.equals("jdbc")) {
+				initJdbcDataSource(pool);
+			} else if (poolType.equals("jndi")) {
+				initJndiDataSource(pool);
+			} else {
+				Log.e("no such connection pool type: " + poolType);
+				break;
+			}
+			Connection conn = getConnection(name);
+			releaseConnection(conn);
+		}
+
+	}
+
+	private static void initJndiDataSource(Element pool) {
+		String name = pool.getAttribute("name");
+		DataSource ds = null;
+		try {
+			Context initCtx = new InitialContext();
+			String javaEnv = pool.getAttribute("jndiprefix");
+			if (javaEnv != null && !javaEnv.equals("")) {
+				String jndiname = pool.getAttribute("jndiname");
+				Context envCtx = (Context) initCtx.lookup(javaEnv);
+				ds = (DataSource) envCtx.lookup(jndiname);
+
+			} else {
+				String jndiname = pool.getAttribute("jndiname");
+				ds = (DataSource) initCtx.lookup(jndiname);
+			}
+			dataSources.put(name, ds);
+		} catch (NamingException e) {
+			Log.e("initJndiDataSource failed", e);
+		}
+	}
+
+	private static void initJdbcDataSource(Element pool) {
+		ComboPooledDataSource pooledDataSource = new ComboPooledDataSource(true);
+		// PoolBackedDataSource p = new PoolBackedDataSource();
+		String name = pool.getAttribute("name");
+		dataSources.put(name, pooledDataSource);
+
+		NodeList paramConfig = pool.getElementsByTagName("param");
+		for (int n = 0; n < paramConfig.getLength(); n++) {
+			Element param = (Element) paramConfig.item(n);
+			String paramName = param.getAttribute("name").toLowerCase();
+			String value = param.getAttribute("value");
+			if (paramName.equals("driver")) {
+				try {
+					pooledDataSource.setDriverClass(value);
+				} catch (PropertyVetoException e) {
+					e.printStackTrace();
+				}
+			} else if (paramName.equals("url")) {
+				pooledDataSource.setJdbcUrl(value);
+			} else if (paramName.equals("user")) {
+				pooledDataSource.setUser(value);
+			} else if (paramName.equals("password")) {
+				pooledDataSource.setPassword(value);
+			} else if (paramName.equals("initial_size")) {
+				pooledDataSource.setInitialPoolSize(Integer.parseInt(value));
+			} else if (paramName.equals("max_size")) {
+				pooledDataSource.setMaxPoolSize(Integer.parseInt(value));
+			} else if (paramName.equals("min_size")) {
+				pooledDataSource.setMinPoolSize(Integer.parseInt(value));
+			} else if (paramName.equals("idle_time")) {
+				pooledDataSource.setMaxIdleTime(Integer.parseInt(value));
+			}
+		}
+		pooledDataSource.setIdleConnectionTestPeriod(60);
+		pooledDataSource.setAutoCommitOnClose(true);
+		pooledDataSource.setTestConnectionOnCheckout(true);
+		pooledDataSource.setTestConnectionOnCheckin(true);
+		pooledDataSource.setAcquireRetryAttempts(300000);
+		pooledDataSource.setAcquireRetryDelay(3000);
+		pooledDataSource.setAutomaticTestTable("INMEMDB_POOL_DO_NOT_DELETE");
+		pooledDataSource.setAutoCommitOnClose(true);
+	}
+
+	public synchronized static Connection getConnection() {
+		return getConnection(defaultSource);
+	}
+
+	public synchronized static Connection getConnection(String poolName) {
+		Connection conn = null;
+		try {
+			DataSource datasource = dataSources.get(poolName);
+			if (datasource == null) {
+				Log.e("cannot get connection, no such datasource: " + poolName);
+				return null;
+			}
+			conn = datasource.getConnection();
+			if (conn != null) {
+				conn.setAutoCommit(false);
+			}
+		} catch (SQLException e) {
+			Log.e("SQL exception " + e.getMessage(), e);
+		}
+		return conn;
+	}
+
+	public synchronized static void releaseConnection(Connection conn) {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				Log.e("release connection failed", e);
+			}
+		}
+	}
+
+	public synchronized static void rollbackConnection(Connection conn) {
+		if (conn != null) {
+			try {
+				conn.rollback();
+			} catch (SQLException e) {
+				Log.e("roll back failed", e);
+			}
+		}
+	}
+
+}
---
+++ src/com/brightedu/util/Log.java
@@ -0,0 +1,65 @@
+package com.brightedu.util;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class Log {
+	static byte[] syncLock = new byte[0];
+
+	private static String FQCN = Log.class.getName();
+	private static Logger logger = Logger.getLogger(Log.class);
+
+	static {
+		String log4jConfigLocation = System.getProperty("log4j.configuration");
+		if (log4jConfigLocation == null) {
+			log4jConfigLocation = Log.class.getResource(
+					"/resources/log4j.properties").getFile();
+		}
+		PropertyConfigurator.configureAndWatch(log4jConfigLocation, 3000);
+	}
+
+	public static void d(String msg) {
+		logger.log(FQCN, Level.DEBUG, msg, null);
+	}
+
+	public static void d(String msg, Throwable e) {
+		logger.log(FQCN, Level.DEBUG, msg, e);
+	}
+	
+	public static void i(String msg) {
+		logger.log(FQCN, Level.INFO, msg, null);
+	}
+
+	public static void i(String msg, Throwable e) {
+		logger.log(FQCN, Level.INFO, msg, e);
+	}
+	
+	public static void w(String msg) {
+		logger.log(FQCN, Level.WARN, msg, null);
+	}
+
+	public static void w(String msg, Throwable e) {
+		logger.log(FQCN, Level.WARN, msg, e);
+	}
+	
+	public static void e(String msg) {
+		logger.log(FQCN, Level.ERROR, msg, null);
+	}
+
+	public static void e(String msg, Throwable e) {
+		logger.log(FQCN, Level.ERROR, msg, e);
+	}
+	
+	public static void f(String msg) {
+		logger.log(FQCN, Level.FATAL, msg, null);
+	}
+
+	public static void f(String msg, Throwable e) {
+		logger.log(FQCN, Level.FATAL, msg, e);
+	}
+
+	public static Logger getLogger() {
+		return logger;
+	}
+}
