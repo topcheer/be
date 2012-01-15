ce2cebb133d3ca36d30d50cb028a9b1f36054e5a
zhanju <zhanju@9987ea41-4527-47ae-bde9-3645be3cca0f>
2012-01-15 10:55:37 +0000

    git-svn-id: svn://jmedu.wassecurity.net/BrightEdu/trunk@13 9987ea41-4527-47ae-bde9-3645be3cca0f

---
+++ test/com/brightedu/dao/edu/TestAgreementDAO.java
@@ -0,0 +1,59 @@
+package com.brightedu.dao.edu;
+
+import static org.junit.Assert.*;
+import java.io.IOException; 
+import java.io.Reader; 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException; 
+import org.apache.ibatis.io.Resources; 
+import org.apache.ibatis.session.SqlSession; 
+import org.apache.ibatis.session.SqlSessionFactory; 
+import org.apache.ibatis.session.SqlSessionFactoryBuilder; 
+
+import org.junit.Test;
+
+import com.brightedu.model.edu.College;
+import com.brightedu.model.edu.CollegeAgreement;
+import com.brightedu.util.ConnectionManager;
+
+public class TestAgreementDAO {
+
+	@Test
+	public void test() throws IOException {
+		
+		
+		String resource = "com/brightedu/MapperConfig.xml"; 
+		Reader reader = Resources.getResourceAsReader(resource);
+		SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(reader); 
+		SqlSession session = sessionFactory.openSession(); 
+	
+		College co = new College();
+		co.setCollege_name("Test Coll");
+		co.setRegister_date(new Date(System.currentTimeMillis()));
+		session.insert("com.brightedu.dao.edu.CollegeMapper.insert",co);
+		session.commit();
+		session.close();
+//		ConnectionManager.load();
+//		try {
+//			Connection conn = ConnectionManager.getConnection("edu");
+//			System.out.print(conn.getAutoCommit());
+//			PreparedStatement ps = conn.prepareStatement("insert into edu.co_college (college_name) values (?)");
+//			ps.setString(1, "Test Col");
+//			ps.execute();
+//			ps.close();	
+//			conn.commit();
+//			conn.close();
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
+				
+		
+	
+	}
+
+}
