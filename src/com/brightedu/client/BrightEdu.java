911fb8a2bdcc3475ffda9d9d9e93f892cd37b974
chet <chet@9987ea41-4527-47ae-bde9-3645be3cca0f>
2012-01-26 07:47:12 +0000

    增加导航树
    git-svn-id: svn://jmedu.wassecurity.net/BrightEdu/trunk@41 9987ea41-4527-47ae-bde9-3645be3cca0f

--- src/com/brightedu/client/BrightEdu.java
+++ src/com/brightedu/client/BrightEdu.java
@@ -1,18 +1,15 @@
 package com.brightedu.client;
 
-import java.util.LinkedHashMap;
-
 import com.brightedu.client.login.LoginDialog;
+import com.brightedu.client.nav.ExplorerTreeNode;
+import com.brightedu.client.nav.FunctionTree;
 import com.brightedu.model.edu.User;
-
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.user.client.Cookies;
 import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.smartgwt.client.types.Alignment;
-import com.smartgwt.client.types.TabBarControls;
 import com.smartgwt.client.types.VerticalAlignment;
 import com.smartgwt.client.util.SC;
 import com.smartgwt.client.widgets.Canvas;
@@ -23,21 +20,18 @@ import com.smartgwt.client.widgets.events.ClickEvent;
 import com.smartgwt.client.widgets.events.ClickHandler;
 import com.smartgwt.client.widgets.events.CloseClickEvent;
 import com.smartgwt.client.widgets.events.CloseClickHandler;
-import com.smartgwt.client.widgets.form.DynamicForm;
-import com.smartgwt.client.widgets.form.fields.SelectItem;
-import com.smartgwt.client.widgets.form.fields.events.ChangeEvent;
-import com.smartgwt.client.widgets.form.fields.events.ChangeHandler;
 import com.smartgwt.client.widgets.layout.HLayout;
 import com.smartgwt.client.widgets.layout.Layout;
-import com.smartgwt.client.widgets.layout.LayoutSpacer;
 import com.smartgwt.client.widgets.layout.VLayout;
-import com.smartgwt.client.widgets.menu.Menu;
 import com.smartgwt.client.widgets.tab.Tab;
 import com.smartgwt.client.widgets.tab.TabSet;
 import com.smartgwt.client.widgets.tab.events.TabSelectedEvent;
 import com.smartgwt.client.widgets.tab.events.TabSelectedHandler;
 import com.smartgwt.client.widgets.toolbar.ToolStrip;
 import com.smartgwt.client.widgets.toolbar.ToolStripButton;
+import com.smartgwt.client.widgets.tree.TreeNode;
+import com.smartgwt.client.widgets.tree.events.LeafClickEvent;
+import com.smartgwt.client.widgets.tree.events.LeafClickHandler;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -59,6 +53,15 @@ public class BrightEdu implements EntryPoint {
 			.create(GreetingService.class);
 
 	private String auth;
+	private static String idSuffix = "idsuffix";
+	private static ExplorerTreeNode[] treeNodeData = new ExplorerTreeNode[]{
+        new ExplorerTreeNode("系统管理", "featured-category", "root", "silk/house.png", null, true, idSuffix),
+        new ExplorerTreeNode("Demo Application", "featured-complete-app", "featured-category", "silk/layout_content.png", null, true, idSuffix),
+        new ExplorerTreeNode("Smart GWT MVC", "featured-smartgwt-mvc", "featured-category", "silk/arrow_join.png", null, true, idSuffix),
+        new ExplorerTreeNode("Grid Cell Widgets", "featured-grid-cell-widgets", "featured-category", null, null, true, idSuffix),
+        new ExplorerTreeNode("Miller Columns", "featured-miller-columns", "featured-category", "silk/ipod.png",null, true, idSuffix),
+        new ExplorerTreeNode("Nested Grid", "featured-nested-grid", "featured-category", "crystal/16/mimetypes/widget_doc.png", null, true, idSuffix),
+	};
 
 	/**
 	 * This is the entry point method.
@@ -251,6 +254,7 @@ public class BrightEdu implements EntryPoint {
 	}
 
 	private TabSet mainTabSet;
+
 	private void createAdminUI() {
 		VLayout main = new VLayout() {
 			@Override
@@ -263,62 +267,58 @@ public class BrightEdu implements EntryPoint {
 		};
 
 		ToolStrip topBar = new ToolStrip();
-		topBar.setHeight(33);
+		topBar.setHeight(62);
 		topBar.setWidth100();
-
+		// topBar。
+		// topBar.addSpacer(6);
+		ImgButton homeBtn = new ImgButton();
+		homeBtn.setSrc("jm_toolstrip.png");
+		// sgwtHomeButton.setWidth(24);
+		// sgwtHomeButton.setHeight(24);
+		homeBtn.setPrompt("今明教育在线");
+		// sgwtHomeButton.setHoverStyle("interactImageHover");
+		homeBtn.setShowRollOver(false);
+		homeBtn.setShowDownIcon(false);
+		homeBtn.setShowDown(false);
+		homeBtn.addClickHandler(new com.smartgwt.client.widgets.events.ClickHandler() {
+			public void onClick(ClickEvent event) {
+				com.google.gwt.user.client.Window.open(
+						"http://www.jmedu.com.cn/", "今明教育在线", null);
+			}
+		});
+		topBar.addMember(homeBtn);
 		topBar.addSpacer(6);
-		ImgButton sgwtHomeButton = new ImgButton();
-		sgwtHomeButton.setSrc("pieces/24/cube_green.png");
-		sgwtHomeButton.setWidth(24);
-		sgwtHomeButton.setHeight(24);
-		sgwtHomeButton.setPrompt("Smart GWT Project Page");
-		sgwtHomeButton.setHoverStyle("interactImageHover");
-		sgwtHomeButton.setShowRollOver(false);
-		sgwtHomeButton.setShowDownIcon(false);
-		sgwtHomeButton.setShowDown(false);
-		sgwtHomeButton
-				.addClickHandler(new com.smartgwt.client.widgets.events.ClickHandler() {
-					public void onClick(ClickEvent event) {
-						com.google.gwt.user.client.Window.open(
-								"http://code.google.com/p/smartgwt/", "sgwt",
-								null);
-					}
-				});
-		topBar.addMember(sgwtHomeButton);
-		topBar.addSpacer(6);
-
-		Label title = new Label("Smart GWT Showcase");
-		title.setStyleName("sgwtTitle");
-		title.setWidth(300);
-		topBar.addMember(title);
 
 		topBar.addFill();
 
-		ToolStripButton devConsoleButton = new ToolStripButton();
-		devConsoleButton.setTitle("Developer Console");
-		devConsoleButton.setIcon("silk/bug.png");
-		devConsoleButton
+		ToolStripButton login_logout_Btn = new ToolStripButton();
+		login_logout_Btn.setTitle("退出");
+		login_logout_Btn.setIcon("logout.gif");
+		login_logout_Btn
 				.addClickHandler(new com.smartgwt.client.widgets.events.ClickHandler() {
 					public void onClick(ClickEvent event) {
-						SC.showConsole();
+						login_logout();
 					}
 				});
 
-		topBar.addButton(devConsoleButton);
+		topBar.addButton(login_logout_Btn);
 
 		topBar.addSeparator();
 
-		ImgButton imgButton = new ImgButton();
-		imgButton.setWidth(18);
-		imgButton.setHeight(18);
-		imgButton.setSrc("silk/emoticon.png");
-		imgButton.setShowFocused(false);
-		imgButton.setShowFocusedIcon(false);
-		imgButton.setPrompt("I'm feeling lucky");
-		imgButton.setHoverWidth(110);
-		imgButton.setHoverStyle("interactImageHover");
+		ToolStripButton aboutBtn = new ToolStripButton();
+		aboutBtn.setWidth(18);
+		aboutBtn.setHeight(18);
+		aboutBtn.setIcon("about.gif");
+		aboutBtn.setShowFocused(false);
+		aboutBtn.setPrompt("帮助");
+		aboutBtn.addClickHandler(new ClickHandler() {
 
-		topBar.addMember(imgButton);
+			@Override
+			public void onClick(ClickEvent event) {
+				SC.say("help content");
+			}
+		});
+		topBar.addMember(aboutBtn);
 
 		topBar.addSpacer(6);
 
@@ -337,16 +337,15 @@ public class BrightEdu implements EntryPoint {
 		sideNavLayout.setHeight100();
 		sideNavLayout.setWidth(185);
 		sideNavLayout.setShowResizeBar(true);
-
-		// sideNav = new SideNavTree();
-		// sideNav.addLeafClickHandler(new LeafClickHandler() {
-		// public void onLeafClick(LeafClickEvent event) {
-		// TreeNode node = event.getLeaf();
-		// showSample(node);
-		// }
-		// });
-		//
-		// sideNavLayout.addMember(sideNav);
+		FunctionTree sideNav = new FunctionTree();
+        sideNav.addLeafClickHandler(new LeafClickHandler() {
+            public void onLeafClick(LeafClickEvent event) {
+                TreeNode node = event.getLeaf();
+                showFunction(node);
+            }
+        });
+        sideNavLayout.addMember(sideNav);
+        
 		hLayout.addMember(sideNavLayout);
 
 		mainTabSet = new TabSet();
@@ -369,44 +368,6 @@ public class BrightEdu implements EntryPoint {
 				}
 			}
 		});
-
-		LayoutSpacer layoutSpacer = new LayoutSpacer();
-		layoutSpacer.setWidth(5);
-
-		SelectItem selectItem = new SelectItem();
-		selectItem.setHeight(21);
-		selectItem.setWidth(130);
-		LinkedHashMap<String, String> valueMap = new LinkedHashMap<String, String>();
-		valueMap.put("EnterpriseBlue", "Enterprise Blue");
-		valueMap.put("Enterprise", "Enterprise Gray");
-		valueMap.put("Graphite", "Graphite");
-		valueMap.put("Simplicity", "Simplicity");
-
-		selectItem.setValueMap(valueMap);
-
-		final String skinCookieName = "skin_name_2_4";
-		String currentSkin = Cookies.getCookie(skinCookieName);
-		if (currentSkin == null) {
-			currentSkin = "Enterprise";
-		}
-		selectItem.setDefaultValue(currentSkin);
-		selectItem.setShowTitle(false);
-		selectItem.addChangeHandler(new ChangeHandler() {
-			public void onChange(ChangeEvent event) {
-				Cookies.setCookie(skinCookieName, (String) event.getValue());
-				com.google.gwt.user.client.Window.Location.reload();
-			}
-		});
-
-		DynamicForm form = new DynamicForm();
-		form.setPadding(0);
-		form.setMargin(0);
-		form.setCellPadding(1);
-		form.setNumCols(1);
-		form.setFields(selectItem);
-
-		mainTabSet.setTabBarControls(TabBarControls.TAB_SCROLLER,
-				TabBarControls.TAB_PICKER, layoutSpacer, form);
 
 		// contextMenu = createContextMenu();
 
@@ -441,7 +402,7 @@ public class BrightEdu implements EntryPoint {
 			label.setHeight(50);
 			label.setValign(VerticalAlignment.CENTER);
 			label.setAlign(Alignment.CENTER);
-			label.setContents("Firebug can make the Showcase run slow. For the best performance, we suggest disabling Firebug for this site.");
+			label.setContents("请对当前站点关闭Firebug");
 
 			Window fbWindow = new Window();
 			fbWindow.setTitle("Firebug Detected");
@@ -452,5 +413,18 @@ public class BrightEdu implements EntryPoint {
 			main.addMember(fbWindow);
 		}
 		main.draw();
+	}
+
+	protected void showFunction(TreeNode node) {
+		
+	}
+
+
+	private void login_logout() {
+		SC.say("login or logout!");
+	}
+
+	public static ExplorerTreeNode[] getNavTreeData() {
+		return treeNodeData;
 	}
 }
