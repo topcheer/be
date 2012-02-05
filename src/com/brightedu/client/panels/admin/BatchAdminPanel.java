d9b3f072107e27e68d6a71bb308190ddc4fd83d1
zhanju <zhanju@9987ea41-4527-47ae-bde9-3645be3cca0f>
2012-02-05 04:49:54 +0000

    git-svn-id: svn://jmedu.wassecurity.net/BrightEdu/trunk@91 9987ea41-4527-47ae-bde9-3645be3cca0f

--- src/com/brightedu/client/panels/admin/BatchAdminPanel.java
+++ src/com/brightedu/client/panels/admin/BatchAdminPanel.java
@@ -1,12 +1,69 @@
 package com.brightedu.client.panels.admin;
 
+import java.util.Iterator;
+import java.util.List;
+
 import com.brightedu.client.panels.BasicAdminPanel;
+import com.brightedu.model.edu.BatchIndex;
+import com.smartgwt.client.types.FieldType;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.smartgwt.client.data.DataSource;
+import com.smartgwt.client.data.DataSourceField;
 import com.smartgwt.client.data.Record;
+import com.smartgwt.client.util.BooleanCallback;
 import com.smartgwt.client.util.SC;
+import com.smartgwt.client.widgets.Dialog;
 import com.smartgwt.client.widgets.form.DynamicForm;
 import com.smartgwt.client.widgets.form.fields.TextItem;
+import com.smartgwt.client.widgets.grid.ListGridRecord;
 
 public class BatchAdminPanel extends BasicAdminPanel {
+	final AdminDialog dialog = new NewBatchDialog();
+	protected void postInit()
+	{
+		
+		final DataSource ds= new DataSource();
+		ds.setClientOnly(true);
+		ds.addField(new DataSourceField("batch_index",FieldType.TEXT,"批次代码"));
+		ds.addField(new DataSourceField("batch_name",FieldType.TEXT,"批次名称"));
+		ds.addField(new DataSourceField("reg_time",FieldType.TEXT,"录入时间"));
+		
+	   AsyncCallback<List<BatchIndex>> callback = new AsyncCallback<List<BatchIndex>>()
+	   {
+
+		@Override
+		public void onFailure(Throwable caught) {
+
+			caught.printStackTrace();
+			
+		}
+
+		@Override
+		public void onSuccess(List<BatchIndex> result) {
+			
+			Iterator<BatchIndex> it = result.iterator();
+			
+			while(it.hasNext())
+			{
+				BatchIndex bi = it.next();
+				Record rec = new Record();
+				rec.setAttribute("batch_index", bi.getBatch_id());
+				rec.setAttribute("batch_name", bi.getBatch_name());
+				rec.setAttribute("reg_time", bi.getRegister_date());
+				ds.addData(rec);
+			}
+		}
+		   
+	   };
+		dbService.getBatchList(callback);
+		
+		resultList.setDataSource(ds);
+		resultList.fetchData();
+		resultList.redraw();
+		
+		
+		
+	}
 
 	@Override
 	protected void search(String keyWords, Record range) {
@@ -14,12 +71,67 @@ public class BatchAdminPanel extends BasicAdminPanel {
 
 	@Override
 	protected void addRecord() {
-		AdminDialog dialog = new NewBatchDialog();
+		
 		dialog.show();		
 	}
 
 	@Override
 	protected void deleteRecords() {
+		
+		if(resultList.getSelectedRecord() == null)
+		{
+			SC.say("请选择一些记录");
+			return;
+		}
+		
+		//final Integer batch_id = resultList.getSelectedRecord().getAttributeAsInt("batch_index");
+		
+		SC.ask("确认", "你确认要删除选中的记录吗？", new BooleanCallback(){
+
+			@Override
+			public void execute(Boolean value) {
+				
+				if(value)
+				{
+					ListGridRecord[] records=resultList.getSelectedRecords();
+					
+					for(int i=0;i<records.length;i++)
+					{
+						dbService.deleteBatch(records[i].getAttributeAsInt("batch_index"), new AsyncCallback<Boolean>()
+						{
+							@Override
+							public void onFailure(Throwable caught) {
+								// TODO Auto-generated method stub
+								
+								caught.printStackTrace();
+								
+							}
+
+							@Override
+							public void onSuccess(Boolean result) {
+								// TODO Auto-generated method stub
+							
+//								if(result.booleanValue())
+//								{
+//									SC.say("成功", "成功删除记录");
+//								}
+//								{
+//									SC.say("失败", "操作失败，请稍后再试");
+//								}
+								
+							}
+						
+					});
+
+					}
+				}
+				postInit();
+					
+				}
+				
+			});
+			
+	
 
 	}
 
@@ -50,8 +162,8 @@ public class BatchAdminPanel extends BasicAdminPanel {
 
 	@Override
 	protected void initPages() {
-		
 
+  
 	}
 
 	private class NewBatchDialog extends AdminDialog {
@@ -75,9 +187,30 @@ public class BatchAdminPanel extends BasicAdminPanel {
 
 		@Override
 		protected void createNewAdminItem() {
-			String batch = batchItem.getValueAsString();
+			final String batch = batchItem.getValueAsString();
 			if (batch != null && batch.trim().length() > 0) {
-				SC.say("新建批次："+batch);
+				dbService.addBatch(batch, new AsyncCallback<Boolean>(){
+
+					@Override
+					public void onFailure(Throwable caught) {
+						// TODO Auto-generated method stub
+						caught.printStackTrace();
+						
+					}
+
+					@Override
+					public void onSuccess(Boolean result) {
+						// TODO Auto-generated method stub
+						SC.say("新建批次："+batch);
+						
+						//refresh GridList
+						postInit();
+						dialog.destroy();
+					}
+					
+				});
+				
+				
 				showLastPageRecords();
 			}else{
 				SC.say("空内容无效！");
