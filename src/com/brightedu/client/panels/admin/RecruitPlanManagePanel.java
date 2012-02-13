0980a4e6347b3b54361779fc62d9527c534d22b4
zhanju <zhanju@9987ea41-4527-47ae-bde9-3645be3cca0f>
2012-02-13 16:52:15 +0000

    git-svn-id: svn://jmedu.wassecurity.net/BrightEdu/trunk@227 9987ea41-4527-47ae-bde9-3645be3cca0f

--- src/com/brightedu/client/panels/admin/RecruitPlanManagePanel.java
+++ src/com/brightedu/client/panels/admin/RecruitPlanManagePanel.java
@@ -20,19 +20,24 @@ import com.smartgwt.client.data.fields.DataSourceIntegerField;
 import com.smartgwt.client.data.fields.DataSourceTextField;
 import com.smartgwt.client.types.Alignment;
 import com.smartgwt.client.types.DragDataAction;
+import com.smartgwt.client.types.ListGridEditEvent;
 import com.smartgwt.client.types.ListGridFieldType;
 import com.smartgwt.client.types.TitleOrientation;
 import com.smartgwt.client.types.VisibilityMode;
 import com.smartgwt.client.util.SC;
 import com.smartgwt.client.widgets.Canvas;
 import com.smartgwt.client.widgets.IButton;
+import com.smartgwt.client.widgets.TransferImgButton;
 import com.smartgwt.client.widgets.events.ClickEvent;
 import com.smartgwt.client.widgets.events.ClickHandler;
 import com.smartgwt.client.widgets.form.DynamicForm;
 import com.smartgwt.client.widgets.form.fields.SelectItem;
 import com.smartgwt.client.widgets.grid.ListGrid;
 import com.smartgwt.client.widgets.grid.ListGridField;
+import com.smartgwt.client.widgets.grid.events.CellDoubleClickEvent;
+import com.smartgwt.client.widgets.grid.events.CellDoubleClickHandler;
 import com.smartgwt.client.widgets.layout.HLayout;
+import com.smartgwt.client.widgets.layout.HStack;
 import com.smartgwt.client.widgets.layout.SectionStack;
 import com.smartgwt.client.widgets.layout.SectionStackSection;
 import com.smartgwt.client.widgets.layout.VLayout;
@@ -45,72 +50,51 @@ public class RecruitPlanManagePanel extends VLayout {
 	{
         //Canvas canvas = new Canvas();   
 		setPadding(5);
-		setAlign(Alignment.CENTER);
 		SectionStack aStack = new SectionStack();   
-		aStack.setWidth("90%");
-		aStack.setHeight100();
+		aStack.setWidth100();
+		aStack.setHeight(600);
 		aStack.setVisibilityMode(VisibilityMode.MULTIPLE);
 		SectionStackSection conditionSection = new SectionStackSection ("条件");
 		SectionStackSection selectionSection = new SectionStackSection ("选择 - 将左边列表中的专业拖动到右边列表中");
 		conditionSection.setExpanded(true);
 		selectionSection.setExpanded(true);
 		
-
-	
-		HLayout queryBar = new HLayout();
-		
-		queryBar.setPadding(3);
-		queryBar.setHeight(40);
-		
 		DynamicForm df = new DynamicForm();
+		df.setHeight(30);
 		df.setWidth(600);
-		df.setColWidths(120,120,120);
+		df.setColWidths(150,150,150);
 		df.setNumCols(3);
 		
 		SelectItem batchItem =  new SelectItem("batchID","批次");
 		batchItem.setOptionDataSource(BatchDS.getInstance());
 		batchItem.setValueField("batchID");
 		batchItem.setDisplayField("batchName");
-		batchItem.setMultiple(true);
+		batchItem.setDefaultToFirstOption(true);
+		
 		
 		SelectItem collegeItem =  new SelectItem("collegeID","大学");
+		collegeItem.setLeft(20);
 		collegeItem.setOptionDataSource(CollegeDS.getInstance());
 		collegeItem.setValueField("collegeID");
 		collegeItem.setDisplayField("collegeName");
-
+		collegeItem.setDefaultToFirstOption(true);
+		
 		SelectItem levelItem =  new SelectItem("levelID","层次");
+		levelItem.setLeft(20);
 		levelItem.setOptionDataSource(LevelDS.getInstance());
 		levelItem.setValueField("levelID");
 		levelItem.setDisplayField("levelName");
-		
-		df.setTitleOrientation(TitleOrientation.TOP);
-		
+		levelItem.setDefaultToFirstOption(true);
 		df.setItems(batchItem,collegeItem,levelItem);
-		queryBar.addMember(df);
-		
-
-		
-		conditionSection.addItem(queryBar);
+		conditionSection.addItem(df);
 		aStack.addSection(conditionSection);
 		
 		
-		
-//		
-//		IButton saveButton = new IButton("保存");
-//		saveButton.setLeft(500);
-//		queryBar.addMember(saveButton);
-//		
-//		saveButton.addClickHandler(new ClickHandler(){
-//			@Override
-//			public void onClick(ClickEvent event) {
-//								
-//			}});
-//		
-		
         HLayout hl = new HLayout();
-        hl.setHeight(500);
+        hl.setPadding(4);
+        hl.setHeight(400);
 		
-        ListGrid subjectList = new ListGrid();   
+        final ListGrid subjectList = new ListGrid();   
         
         subjectList.setWidth(300);   
         subjectList.setHeight(350);   
@@ -124,16 +108,31 @@ public class RecruitPlanManagePanel extends VLayout {
         ListGridField subjectField = new ListGridField("subjectName", "专业");   
         pkField.setHidden(true);
         ListGridField lolField = new ListGridField("lol", "学制(年)");  
+        
+        lolField.setCanEdit(true);
+        
         subjectList.setFields(pkField, subjectField);   
         subjectList.setDataSource(SubjectDS.getInstance());
+        
+ 
 
         hl.addMember(subjectList);
         
+        final ListGrid selectedList = new ListGrid();  
         
-        ListGrid selectedList = new ListGrid();   
+       TransferImgButton arrowImg = new TransferImgButton(TransferImgButton.RIGHT);   
+        arrowImg.addClickHandler(new ClickHandler() {   
+            public void onClick(ClickEvent event) {   
+            	selectedList.transferSelectedData(subjectList);   
+            }   
+        }); 
+        arrowImg.setLeft(10);
+        arrowImg.setRight(10);
+        hl.addMember(arrowImg);  
+        
         selectedList.setWidth(350);   
         selectedList.setHeight(350);   
-        selectedList.setLeft(350);   
+        //selectedList.setLeft(400);   
         selectedList.setShowAllRecords(true);   
         selectedList.setEmptyMessage("拖动左边的项目到这里");   
         selectedList.setCanReorderFields(true);   
@@ -142,19 +141,60 @@ public class RecruitPlanManagePanel extends VLayout {
         selectedList.setCanRemoveRecords(true);
         selectedList.setDragDataAction(DragDataAction.MOVE);   
         selectedList.setFields(pkField, subjectField,lolField);   
-        
+        selectedList.setEditEvent(ListGridEditEvent.CLICK);
+        selectedList.setPreventDuplicates(true);
+        selectedList.setDuplicateDragMessage("所选专业已经在已选择列表中,请仔细检查");
         
         hl.addMember(selectedList);
+        
         //canvas.addChild(countryGrid2); 
         selectionSection.addItem(hl);
-        
         aStack.addSection(selectionSection);
+        
+        SectionStackSection actionSection = new SectionStackSection ("操作");
+        actionSection.setShowHeader(true);
+        actionSection.setExpanded(true);
+        
+        HStack buttonStack = new HStack();
+        buttonStack.setHeight(50);
+        buttonStack.setLayoutMargin(10);
+        
+		IButton saveButton = new IButton("保存");
+		saveButton.setLeft(30);
+		buttonStack.addMember(saveButton);
+
+		IButton cloneButton = new IButton("克隆...");
+		cloneButton.setLeft(30);
+		buttonStack.addMember(cloneButton);
+		
+		actionSection.addItem(buttonStack);
+		aStack.addSection(actionSection);
+//		
+//		saveButton.addClickHandler(new ClickHandler(){
+//			@Override
+//			public void onClick(ClickEvent event) {
+//								
+//			}});
+//		
+        
+        
+        
+        
+        
         addMember(aStack);
         //addMember(hl);
         
         draw();
         
         subjectList.fetchData();
+        
+		subjectList.addCellDoubleClickHandler(new CellDoubleClickHandler(){
+
+			@Override
+			public void onCellDoubleClick(CellDoubleClickEvent event) {
+				selectedList.transferSelectedData(subjectList);
+				
+			}});
         //canvas.draw();   
 
 	}
