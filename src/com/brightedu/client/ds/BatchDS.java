74e9f34e29c3862c1aead6523dce1ea058cf8ce8
zhanju <zhanju@9987ea41-4527-47ae-bde9-3645be3cca0f>
2012-02-16 07:23:41 +0000

    git-svn-id: svn://jmedu.wassecurity.net/BrightEdu/trunk@260 9987ea41-4527-47ae-bde9-3645be3cca0f

--- src/com/brightedu/client/ds/BatchDS.java
+++ src/com/brightedu/client/ds/BatchDS.java
@@ -1,7 +1,6 @@
 package com.brightedu.client.ds;
 
 import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.List;
 
 import com.brightedu.client.BrightEdu;
@@ -12,12 +11,11 @@ import com.smartgwt.client.data.Record;
 import com.smartgwt.client.data.fields.DataSourceIntegerField;
 import com.smartgwt.client.data.fields.DataSourceTextField;
 import com.smartgwt.client.util.SC;
-import com.smartgwt.client.widgets.form.fields.SelectItem;
 
 public class BatchDS extends DataSource{
 
     private static BatchDS instance = null;   
-
+    
     public static BatchDS getInstance() {   
     	if(instance != null) instance.destroy(); 
             instance = new BatchDS("batchDS");   
@@ -66,42 +64,5 @@ public class BatchDS extends DataSource{
         	
         });
 	}
-	
-//	public static void setValueMap(final SelectItem item)
-//	{
-//		 
-//		final LinkedHashMap<String,String> valueMap = new LinkedHashMap<String,String>();
-//		
-//        BrightEdu.createDataBaseRPC().getBatchList(-1, -1, false, new AsyncCallback<List<BatchIndex>>(){
-//
-//			@Override
-//			public void onFailure(Throwable caught) {
-//				// TODO Auto-generated method stub
-//				SC.say("获取批次时发生错误");
-//			}
-//
-//			@Override
-//			public void onSuccess(List<BatchIndex> result) {
-//				// TODO Auto-generated method stub
-//				Iterator<BatchIndex> biit = result.iterator();
-//				while(biit.hasNext())
-//				{
-//					BatchIndex bi = biit.next();
-////					Record rc = new Record();
-////					rc.setAttribute("batchID",bi.getBatch_id() );
-////					rc.setAttribute("batchName", bi.getBatch_name());
-//					valueMap.put(bi.getBatch_id() + "", bi.getBatch_name());
-//					System.out.println(valueMap.toString());
-//				}
-//				item.setValueMap(valueMap);
-//				
-//			}
-//        	
-//        });
-//		
-//		
-//		
-//		
-//	}
 	
 }
