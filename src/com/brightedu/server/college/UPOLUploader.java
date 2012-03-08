baeb35c40412d4adebd763a2f3347f4570230c12
zhanju <zhanju@9987ea41-4527-47ae-bde9-3645be3cca0f>
2012-03-08 16:43:38 +0000

    git-svn-id: svn://jmedu.wassecurity.net/BrightEdu/trunk@464 9987ea41-4527-47ae-bde9-3645be3cca0f

--- src/com/brightedu/server/college/UPOLUploader.java
+++ src/com/brightedu/server/college/UPOLUploader.java
@@ -2,6 +2,7 @@ package com.brightedu.server.college;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -9,6 +10,7 @@ import java.util.List;
 
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
@@ -39,9 +41,21 @@ import com.brightedu.shared.IdcardUtils;
  */
 public class UPOLUploader implements IUploader{
 
+	private static final String CONNECTION = "Keep-Alive";
+	private static final String ACCEPT_ENCODING="gzip, deflate";
+	private static final String ACCEPT="image/jpeg, application/x-ms-application, image/gif, application/xaml+xml, image/pjpeg, application/x-ms-xbap, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/msword, application/x-shockwave-flash, */*";
+	private static final String USER_AGENT="Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; QQWubi 87; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)";
+	private static final String ACCEPT_LANGUAGE="zh-CN";
+	private static final String HOST="www.etju.com";
+	private static final String LOGIN_URL = "http://www.etju.com/netfiles3web/(y5ene1rpiarkyjnd132p4gjv)/ctlogin.aspx";
+	private static final String LOGIN_FORM_CONTENT_TYPE = "application/x-www-form-urlencoded";
+	private static final String REG_URL = "http://www.etju.com/netfiles3web/(y5ene1rpiarkyjnd132p4gjv)/CTXLSign.aspx";
+	private static final String LIST_URL = "http://www.etju.com/netfiles3web/(y5ene1rpiarkyjnd132p4gjv)/CTXLView.aspx";
+	
 	private static HashMap<String,UPOLUploader> _instances = new HashMap<String,UPOLUploader>();
 	private RecruitAgent _agent;
 	private DefaultHttpClient client;
+	
 	public static UPOLUploader getInstance(RecruitAgent agent)
 	{
 		
@@ -70,55 +84,43 @@ public class UPOLUploader implements IUploader{
 		// TODO 代码
 		
 		client = new DefaultHttpClient();
-		HttpHost proxy = new HttpHost("127.0.0.1", 8080, "http");
 		
+		
+		//测试环境要用proxy
+		HttpHost proxy = new HttpHost("127.0.0.1", 8080, "http");
 		client.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxy);
 		
 		
 		//http://www.etju.com/netfiles3web/(y5ene1rpiarkyjnd132p4gjv)/ctlogin.aspx用户名：hzzx密码：zjyd 
         try {
            
-            HttpHost target = new HttpHost("www.etju.com", 80, "http");
-            HttpGet req = new HttpGet("/netfiles3web/(y5ene1rpiarkyjnd132p4gjv)/ctlogin.aspx");
-            req.setHeader("Accept", "image/jpeg, application/x-ms-application, image/gif, application/xaml+xml, image/pjpeg, application/x-ms-xbap, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/msword, application/x-shockwave-flash, */*");   
-            req.setHeader("Accept-Language","zh-CN");   
-            req.setHeader("User-Agent","Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; QQWubi 87; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)");   
-            req.setHeader("Accept-Encoding","gzip, deflate");   
-            req.setHeader("Host","www.etju.com");   
-            req.setHeader("Connection","Keep-Alive");   
-
-            System.out.println("executing request to " + target + " via " + proxy);
-            HttpResponse rsp = client.execute(target, req);
+            
+            HttpGet req = new HttpGet(LOGIN_URL);
+            req.setHeader(HttpHeaders.ACCEPT, ACCEPT);   
+            req.setHeader(HttpHeaders.ACCEPT_LANGUAGE,ACCEPT_LANGUAGE);   
+            req.setHeader(HttpHeaders.USER_AGENT,USER_AGENT);   
+            req.setHeader(HttpHeaders.ACCEPT_ENCODING,ACCEPT_ENCODING);   
+            req.setHeader(HttpHeaders.HOST,HOST);   
+            req.setHeader(HttpHeaders.CONNECTION,CONNECTION);   
+            
+            HttpResponse rsp = client.execute(req);
             HttpEntity entity = rsp.getEntity();
 
-            System.out.println("----------------------------------------");
-            System.out.println(rsp.getStatusLine());
-            Header[] headers = rsp.getAllHeaders();
-            for (int i = 0; i<headers.length; i++) {
-                System.out.println(headers[i]);
-            }
-            System.out.println("----------------------------------------");
-            String content = null;
-            if (entity != null) {
-            	content = EntityUtils.toString(entity);
-                System.out.println(content);
-            }
-            
-            String view_state = content.substring(content.indexOf("\"__VIEWSTATE\" value=\"") + 21,
-            		content.indexOf("\" />",content.indexOf("\"__VIEWSTATE\" value=\"") + 21)
-            		) ;
+            String content = EntityUtils.toString(entity);
+
+            String view_state = getViewState(content);
            
             System.out.println(view_state);
             
-            HttpPost req2 = new HttpPost("http://www.etju.com/netfiles3web/(y5ene1rpiarkyjnd132p4gjv)/ctlogin.aspx");
-            req2.setHeader("Referer","http://www.etju.com/netfiles3web/(y5ene1rpiarkyjnd132p4gjv)/ctlogin.aspx");
-            req2.setHeader("Accept", "image/jpeg, application/x-ms-application, image/gif, application/xaml+xml, image/pjpeg, application/x-ms-xbap, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/msword, application/x-shockwave-flash, */*");   
-            req2.setHeader("Accept-Language","zh-CN");   
-            req2.setHeader("User-Agent","Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; QQWubi 87; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)");   
-            req2.setHeader("Accept-Encoding","gzip, deflate");   
-            req2.setHeader("Host","www.etju.com");   
-            req2.setHeader("Connection","Keep-Alive");  
-            req2.setHeader("Content-Type","application/x-www-form-urlencoded");
+            HttpPost req2 = new HttpPost(LOGIN_URL);
+            req2.setHeader(HttpHeaders.REFERER,LOGIN_URL);
+            req2.setHeader(HttpHeaders.ACCEPT, ACCEPT);   
+            req2.setHeader(HttpHeaders.ACCEPT_LANGUAGE,ACCEPT_LANGUAGE);   
+            req2.setHeader(HttpHeaders.USER_AGENT,USER_AGENT);   
+            req2.setHeader(HttpHeaders.ACCEPT_ENCODING,ACCEPT_ENCODING);   
+            req2.setHeader(HttpHeaders.HOST,HOST);   
+            req2.setHeader(HttpHeaders.CONNECTION,CONNECTION); 
+            req2.setHeader(HttpHeaders.CONTENT_TYPE,LOGIN_FORM_CONTENT_TYPE);
 
             
             List <NameValuePair> nvps = new ArrayList <NameValuePair>();
@@ -130,66 +132,36 @@ public class UPOLUploader implements IUploader{
 
             req2.setEntity(new UrlEncodedFormEntity(nvps, HTTP.ISO_8859_1));
             
-            HttpResponse rsp2 = client.execute(req2);
-            HttpEntity entity2 = rsp2.getEntity();
+            rsp = client.execute(req2);
+            entity = rsp.getEntity();
 
-            System.out.println("Login form get: " + rsp2.getStatusLine());
-            System.out.println(EntityUtils.toString(entity2));
+            content = EntityUtils.toString(entity);
 
-            Header[] headers2 = rsp.getAllHeaders();
-            for (int i = 0; i<headers2.length; i++) {
-                System.out.println(headers2[i]);
-            }
-          //GET http://www.etju.com/netfiles3web/(y5ene1rpiarkyjnd132p4gjv)/CTXLSign.aspx HTTP/1.1
-          //Accept: application/x-shockwave-flash, image/gif, image/x-xbitmap, image/jpeg, image/pjpeg, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/msword, */*
-          //Referer: http://www.etju.com/netfiles3web/(y5ene1rpiarkyjnd132p4gjv)/CTXLView.aspx
-          //Accept-Language: en-gb
-          //Accept-Encoding: gzip, deflate
-          //User-Agent: Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)
-          //Host: www.etju.com
-          //Proxy-Connection: Keep-Alive
-          //Pragma: no-cache
-            HttpGet req3 = new HttpGet("http://www.etju.com/netfiles3web/(y5ene1rpiarkyjnd132p4gjv)/CTXLSign.aspx");
-            req3.setHeader("Referer","http://www.etju.com/netfiles3web/(y5ene1rpiarkyjnd132p4gjv)/CTXLView.aspx");
-            req3.setHeader("Accept", "image/jpeg, application/x-ms-application, image/gif, application/xaml+xml, image/pjpeg, application/x-ms-xbap, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/msword, application/x-shockwave-flash, */*");   
-            req3.setHeader("Accept-Language","zh-CN");   
-            req3.setHeader("User-Agent","Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; QQWubi 87; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)");   
-            req3.setHeader("Accept-Encoding","gzip, deflate");   
-            req3.setHeader("Host","www.etju.com");   
-            req3.setHeader("Connection","Keep-Alive");  
-//            req3.setHeader("Content-Type","application/x-www-form-urlencoded");
+            req = new HttpGet(REG_URL);
+            req.setHeader(HttpHeaders.REFERER,LIST_URL);
+            req.setHeader(HttpHeaders.ACCEPT, ACCEPT);   
+            req.setHeader(HttpHeaders.ACCEPT_LANGUAGE,ACCEPT_LANGUAGE);   
+            req.setHeader(HttpHeaders.USER_AGENT,USER_AGENT);   
+            req.setHeader(HttpHeaders.ACCEPT_ENCODING,ACCEPT_ENCODING);   
+            req.setHeader(HttpHeaders.HOST,HOST);   
+            req.setHeader(HttpHeaders.CONNECTION,CONNECTION);   
 
-            HttpResponse rsp3 = client.execute(req3);
-            HttpEntity entity3 = rsp3.getEntity();
-            content = EntityUtils.toString(entity3);
-            System.out.println("Server status: " + rsp3.getStatusLine());
-            System.out.println(content);
+            rsp = client.execute(req);
+            entity = rsp.getEntity();
+            content = EntityUtils.toString(entity);
 
-            view_state = content.substring(content.indexOf("\"__VIEWSTATE\" value=\"") + 21,
-            		content.indexOf("\" />",content.indexOf("\"__VIEWSTATE\" value=\"") + 21)
-            		) ;
+            view_state = getViewState(content);
             
-//            POST http://www.etju.com/netfiles3web/(y5ene1rpiarkyjnd132p4gjv)/CTXLSign.aspx HTTP/1.1
-//            	Accept: application/x-shockwave-flash, image/gif, image/x-xbitmap, image/jpeg, image/pjpeg, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/msword, */*
-//            	Referer: http://www.etju.com/netfiles3web/(y5ene1rpiarkyjnd132p4gjv)/CTXLSign.aspx
-//            	Accept-Language: en-gb
-//            	Content-Type: multipart/form-data; boundary=---------------------------7dc1302913014c
-//            	Accept-Encoding: gzip, deflate
-//            	User-Agent: Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)
-//            	Host: www.etju.com
-//            	Content-Length: 22348
-//            	Proxy-Connection: Keep-Alive
-//            	Pragma: no-cache
 
-            HttpPost req4 = new HttpPost("http://www.etju.com/netfiles3web/(y5ene1rpiarkyjnd132p4gjv)/CTXLSign.aspx");
+            req2 = new HttpPost(REG_URL);
             
-            req4.setHeader("Referer","http://www.etju.com/netfiles3web/(y5ene1rpiarkyjnd132p4gjv)/CTXLSign.aspx");
-            req4.setHeader("Accept", "image/jpeg, application/x-ms-application, image/gif, application/xaml+xml, image/pjpeg, application/x-ms-xbap, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/msword, application/x-shockwave-flash, */*");   
-            req4.setHeader("Accept-Language","zh-CN");   
-            req4.setHeader("User-Agent","Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; QQWubi 87; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)");   
-            req4.setHeader("Accept-Encoding","gzip, deflate");   
-            req4.setHeader("Host","www.etju.com");   
-            req4.setHeader("Connection","Keep-Alive"); 
+            req2.setHeader(HttpHeaders.REFERER,REG_URL);
+            req2.setHeader(HttpHeaders.ACCEPT, ACCEPT);   
+            req2.setHeader(HttpHeaders.ACCEPT_LANGUAGE,ACCEPT_LANGUAGE);   
+            req2.setHeader(HttpHeaders.USER_AGENT,USER_AGENT);   
+            req2.setHeader(HttpHeaders.ACCEPT_ENCODING,ACCEPT_ENCODING);   
+            req2.setHeader(HttpHeaders.HOST,HOST);   
+            req2.setHeader(HttpHeaders.CONNECTION,CONNECTION); 
             
             MultipartEntity entity4 = new MultipartEntity(); 
 
@@ -244,12 +216,12 @@ public class UPOLUploader implements IUploader{
 //            FileBody fileBody = new FileBody(new File(""),"application/octet-stream"); 
 //            entity4.addPart("UploadFile", fileBody);
             
-            req4.setEntity(entity4);
+            req2.setEntity(entity4);
             
-            HttpResponse rsp4 = client.execute(req4);
-            HttpEntity entity5 = rsp4.getEntity();
-            content = EntityUtils.toString(entity5);
-            System.out.println("Server status: " + rsp4.getStatusLine());
+            rsp = client.execute(req2);
+            entity = rsp.getEntity();
+            content = EntityUtils.toString(entity);
+           
             System.out.println(content);
             
             if(content.contains("请您核对并且打印报名确认表")) 
@@ -280,6 +252,12 @@ public class UPOLUploader implements IUploader{
 		
 			
 		return false;
+	}
+
+	private String getViewState(String content) {
+		return content.substring(content.indexOf("\"__VIEWSTATE\" value=\"") + 21,
+        		content.indexOf("\" />",content.indexOf("\"__VIEWSTATE\" value=\"") + 21)
+        		) ;
 	}
 
 }
