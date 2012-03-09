397d4bc5609e21685f9230ee3eb72be2b20dce50
zhanju <zhanju@9987ea41-4527-47ae-bde9-3645be3cca0f>
2012-03-09 08:22:40 +0000

    git-svn-id: svn://jmedu.wassecurity.net/BrightEdu/trunk@470 9987ea41-4527-47ae-bde9-3645be3cca0f

--- test/com/brightedu/util/TestConnectionManager.java
+++ test/com/brightedu/util/TestConnectionManager.java
@@ -6,13 +6,15 @@ import java.sql.SQLException;
 import org.junit.Test;
 
 import com.brightedu.server.util.ConnectionManager;
+import com.brightedu.shared.IDCard;
 
 public class TestConnectionManager {
 
 	@Test
 	public void test() {
 
-		ConnectionManager.load();
+		System.out.println(IDCard.verify("320623197512156257"));
+//		ConnectionManager.load();
 		// try {
 		// Connection conn = ConnectionManager.getConnection("edu");
 		// System.out.print(conn.getAutoCommit());
