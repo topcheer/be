fc0cd7b6542d86f5c342eff36c49b512efac2b80
zhanju <zhanju@9987ea41-4527-47ae-bde9-3645be3cca0f>
2012-03-09 03:40:29 +0000

    git-svn-id: svn://jmedu.wassecurity.net/BrightEdu/trunk@465 9987ea41-4527-47ae-bde9-3645be3cca0f

--- src/com/brightedu/server/college/UPOLUploader.java
+++ src/com/brightedu/server/college/UPOLUploader.java
@@ -2,26 +2,21 @@ package com.brightedu.server.college;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URI;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHeaders;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.ParseException;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.params.ConnRoutePNames;
-import org.apache.http.cookie.Cookie;
-import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntity;
 import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.entity.mime.content.StringBody;
