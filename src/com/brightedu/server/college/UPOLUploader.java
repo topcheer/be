1cf2019b029e29fa3ec873c32e62f887d4c8f86c
zhanju <zhanju@9987ea41-4527-47ae-bde9-3645be3cca0f>
2012-03-12 09:04:29 +0000

    git-svn-id: svn://jmedu.wassecurity.net/BrightEdu/trunk@484 9987ea41-4527-47ae-bde9-3645be3cca0f

--- src/com/brightedu/server/college/UPOLUploader.java
+++ src/com/brightedu/server/college/UPOLUploader.java
@@ -1,12 +1,15 @@
 package com.brightedu.server.college;
 
-import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import javax.imageio.ImageIO;
+
+import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHeaders;
 import org.apache.http.HttpHost;
@@ -18,7 +21,6 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.params.ConnRoutePNames;
 import org.apache.http.entity.mime.MultipartEntity;
-import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
@@ -28,28 +30,34 @@ import org.apache.http.util.EntityUtils;
 import com.brightedu.model.edu.RecruitAgent;
 import com.brightedu.model.edu.StudentInfo;
 import com.brightedu.shared.IdcardUtils;
+import com.brightedu.util.NumberVerCodeIdentifier;
 /**
- * 石油大学上传接口 
  * 
+ * 天津大学上传接口
  * @author junzhang
  *
  */
-public class UPOLUploader implements IUploader{
-
+public class UPOLUploader implements IUploader {
+	
 	private static final String CONNECTION = "Keep-Alive";
 	private static final String ACCEPT_ENCODING="gzip, deflate";
-	private static final String ACCEPT="image/jpeg, application/x-ms-application, image/gif, application/xaml+xml, image/pjpeg, application/x-ms-xbap, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/msword, application/x-shockwave-flash, */*";
+	private static final String ACCEPT="*/*";
 	private static final String USER_AGENT="Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; QQWubi 87; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)";
 	private static final String ACCEPT_LANGUAGE="zh-CN";
-	private static final String HOST="www.etju.com";
-	private static final String LOGIN_URL = "http://www.etju.com/netfiles3web/(y5ene1rpiarkyjnd132p4gjv)/ctlogin.aspx";
+	private static final String HOST="xueli.upol.cn";
+	private static final String INDEX_URL= "http://xueli.upol.cn/M4/upol/platform/index.jsp";
+	private static final String LOGIN_URL = "http://xueli.upol.cn/M4/upol/platform/login.jsp";
+	private static final String IMG_URL = "http://xueli.upol.cn/M4/upol/platform/image.jsp";
+	private static final String MAIN_URL = "http://xueli.upol.cn/M4/entity/snsenter.jsp?type=SITEMANAGER";
 	private static final String LOGIN_FORM_CONTENT_TYPE = "application/x-www-form-urlencoded";
-	private static final String REG_URL = "http://www.etju.com/netfiles3web/(y5ene1rpiarkyjnd132p4gjv)/CTXLSign.aspx";
-	private static final String LIST_URL = "http://www.etju.com/netfiles3web/(y5ene1rpiarkyjnd132p4gjv)/CTXLView.aspx";
+	private static final String REG_URL = "http://xueli.upol.cn/M4/entity/submanager/recruit/sign_up.jsp";
+	private static final String REG_EXE_URL = "http://xueli.upol.cn/M4/entity/submanager/recruit/sign_upexe.jsp";
+	
+	private DefaultHttpClient client;
 	
 	private static HashMap<String,UPOLUploader> _instances = new HashMap<String,UPOLUploader>();
 	private RecruitAgent _agent;
-	private DefaultHttpClient client;
+	
 	
 	public static UPOLUploader getInstance(RecruitAgent agent)
 	{
@@ -74,9 +82,9 @@ public class UPOLUploader implements IUploader{
 	{
 		_agent = agent;
 	}
+
 	@Override
 	public boolean upload(StudentInfo student) {
-		// TODO 代码
 		
 		client = new DefaultHttpClient();
 		
@@ -90,7 +98,7 @@ public class UPOLUploader implements IUploader{
         try {
            
             
-            HttpGet req = new HttpGet(LOGIN_URL);
+            HttpGet req = new HttpGet(INDEX_URL);
             req.setHeader(HttpHeaders.ACCEPT, ACCEPT);   
             req.setHeader(HttpHeaders.ACCEPT_LANGUAGE,ACCEPT_LANGUAGE);   
             req.setHeader(HttpHeaders.USER_AGENT,USER_AGENT);   
@@ -102,13 +110,50 @@ public class UPOLUploader implements IUploader{
             HttpEntity entity = rsp.getEntity();
 
             String content = EntityUtils.toString(entity);
-
-            String view_state = getViewState(content);
-           
-            System.out.println(view_state);
+            
+            String cookie=null;
+            for (Header h: rsp.getAllHeaders())
+            {
+            	System.out.println(h.getName() + ":" + h.getValue());
+            	if(h.getName().equalsIgnoreCase("Set-Cookie"))
+            	{
+            		cookie = h.getValue().substring(0,h.getValue().indexOf(";"));
+            	}
+            	
+            }
+            System.out.println(cookie);
+            
+//            
+//            String view_state = getViewState(content);
+//           
+//            System.out.println(view_state);
+            String rand = null;
+            while(true)
+            {
+                HttpGet imgreq = new HttpGet(IMG_URL);
+                imgreq.setHeader(HttpHeaders.ACCEPT, ACCEPT);   
+                imgreq.setHeader(HttpHeaders.ACCEPT_LANGUAGE,ACCEPT_LANGUAGE);   
+                imgreq.setHeader(HttpHeaders.USER_AGENT,USER_AGENT);   
+                imgreq.setHeader(HttpHeaders.ACCEPT_ENCODING,ACCEPT_ENCODING);   
+                imgreq.setHeader(HttpHeaders.HOST,HOST);   
+                imgreq.setHeader(HttpHeaders.CONNECTION,CONNECTION);  
+                imgreq.setHeader("Cookie",cookie);
+                HttpResponse imgrsp = client.execute(imgreq);
+                
+                HttpEntity img = imgrsp.getEntity();
+                
+            	rand = NumberVerCodeIdentifier.recognize(ImageIO.read(img.getContent()));
+            	if(rand != null && rand.length() == 4)
+            	{
+            		break;
+            	}
+            		
+            }
+            
+            System.out.println(rand);
             
             HttpPost req2 = new HttpPost(LOGIN_URL);
-            req2.setHeader(HttpHeaders.REFERER,LOGIN_URL);
+            req2.setHeader(HttpHeaders.REFERER,INDEX_URL);
             req2.setHeader(HttpHeaders.ACCEPT, ACCEPT);   
             req2.setHeader(HttpHeaders.ACCEPT_LANGUAGE,ACCEPT_LANGUAGE);   
             req2.setHeader(HttpHeaders.USER_AGENT,USER_AGENT);   
@@ -116,14 +161,13 @@ public class UPOLUploader implements IUploader{
             req2.setHeader(HttpHeaders.HOST,HOST);   
             req2.setHeader(HttpHeaders.CONNECTION,CONNECTION); 
             req2.setHeader(HttpHeaders.CONTENT_TYPE,LOGIN_FORM_CONTENT_TYPE);
-
+            req2.setHeader("Cookie",cookie);
             
             List <NameValuePair> nvps = new ArrayList <NameValuePair>();
-            nvps.add(new BasicNameValuePair("TextBox1", "hzzx"));
-            nvps.add(new BasicNameValuePair("TextBox2", "zjyd"));
-            nvps.add(new BasicNameValuePair("__VIEWSTATE",view_state));
-            nvps.add(new BasicNameValuePair("ImageButton1.x","0"));
-            nvps.add(new BasicNameValuePair("ImageButton1.y","0"));
+            nvps.add(new BasicNameValuePair("login_id", "0485"));
+            nvps.add(new BasicNameValuePair("input_password", "zjjmedu"));
+            nvps.add(new BasicNameValuePair("type","SITEMANAGER"));
+            nvps.add(new BasicNameValuePair("rand",rand));
 
             req2.setEntity(new UrlEncodedFormEntity(nvps, HTTP.ISO_8859_1));
             
@@ -131,24 +175,32 @@ public class UPOLUploader implements IUploader{
             entity = rsp.getEntity();
 
             content = EntityUtils.toString(entity);
+            
+            if(!content.contains(MAIN_URL))
+            {
+            	System.out.println("登录失败");
+            	return false;
+            }
 
-            req = new HttpGet(REG_URL);
-            req.setHeader(HttpHeaders.REFERER,LIST_URL);
+            
+            req = new HttpGet(MAIN_URL);
+            req.setHeader(HttpHeaders.REFERER,LOGIN_URL);
             req.setHeader(HttpHeaders.ACCEPT, ACCEPT);   
             req.setHeader(HttpHeaders.ACCEPT_LANGUAGE,ACCEPT_LANGUAGE);   
             req.setHeader(HttpHeaders.USER_AGENT,USER_AGENT);   
             req.setHeader(HttpHeaders.ACCEPT_ENCODING,ACCEPT_ENCODING);   
             req.setHeader(HttpHeaders.HOST,HOST);   
             req.setHeader(HttpHeaders.CONNECTION,CONNECTION);   
-
+            req.setHeader("Cookie",cookie);
+            
             rsp = client.execute(req);
             entity = rsp.getEntity();
             content = EntityUtils.toString(entity);
 
-            view_state = getViewState(content);
+//            view_state = getViewState(content);
             
 
-            req2 = new HttpPost(REG_URL);
+            req2 = new HttpPost(REG_EXE_URL);
             
             req2.setHeader(HttpHeaders.REFERER,REG_URL);
             req2.setHeader(HttpHeaders.ACCEPT, ACCEPT);   
@@ -157,61 +209,40 @@ public class UPOLUploader implements IUploader{
             req2.setHeader(HttpHeaders.ACCEPT_ENCODING,ACCEPT_ENCODING);   
             req2.setHeader(HttpHeaders.HOST,HOST);   
             req2.setHeader(HttpHeaders.CONNECTION,CONNECTION); 
+            req2.setHeader(HttpHeaders.CONTENT_TYPE,LOGIN_FORM_CONTENT_TYPE);
+            req2.setHeader("Cookie",cookie);
             
-            MultipartEntity entity4 = new MultipartEntity(); 
+            		
+            List <NameValuePair> nvps2 = new ArrayList <NameValuePair>();
+            nvps2.add(new BasicNameValuePair("name", "测试学生"));
+            nvps2.add(new BasicNameValuePair("gender", "男"));
+            nvps2.add(new BasicNameValuePair("folk","汉族"));
+            nvps2.add(new BasicNameValuePair("birthday","1975-12-15"));
+            nvps2.add(new BasicNameValuePair("zzmm","共青团员"));
+            nvps2.add(new BasicNameValuePair("zylb","专业技术人员"));
+            nvps2.add(new BasicNameValuePair("edu","高中毕业"));
+            nvps2.add(new BasicNameValuePair("edutype_id","8"));
+            nvps2.add(new BasicNameValuePair("premajor_status","1"));
+            nvps2.add(new BasicNameValuePair("major_id","06"));
+            nvps2.add(new BasicNameValuePair("card_type","身份证"));
+            nvps2.add(new BasicNameValuePair("card_no","320623197512156257"));
+            nvps2.add(new BasicNameValuePair("postalcode","310000"));
+            nvps2.add(new BasicNameValuePair("address","test address"));
+            nvps2.add(new BasicNameValuePair("gzdw",""));
+            nvps2.add(new BasicNameValuePair("mobilephone","13958222222"));
+            nvps2.add(new BasicNameValuePair("phone",""));
+            nvps2.add(new BasicNameValuePair("email",""));
+            nvps2.add(new BasicNameValuePair("school_name","test school"));
+            nvps2.add(new BasicNameValuePair("school_code","10001"));
+            nvps2.add(new BasicNameValuePair("nian1","1999"));
+            nvps2.add(new BasicNameValuePair("yue1","08"));
+            nvps2.add(new BasicNameValuePair("ri1","11"));
+            nvps2.add(new BasicNameValuePair("graduate_cardno","xxxxxxx"));
+            nvps2.add(new BasicNameValuePair("xxly","他人介绍"));
+            nvps2.add(new BasicNameValuePair("considertype","0"));
+            
 
-            entity4.addPart("__VIEWSTATE", new StringBody(view_state));
-            entity4.addPart("date", new StringBody(""));
-            entity4.addPart("month", new StringBody(""));
-            entity4.addPart("year", new StringBody(""));
-            entity4.addPart("TextBox1", new StringBody("姓名",Charset.forName("GBK")));
-            entity4.addPart("TextBox2", new StringBody("320623197512156257"));
-            entity4.addPart("DropDownList1", new StringBody(IdcardUtils.getGenderByIdCard("320623197512156257"),Charset.forName("GBK")));
-            entity4.addPart("TextBox19", new StringBody(IdcardUtils.getBirthByIdCard("320623197512156257")));
-            entity4.addPart("DropDownList3", new StringBody("共产主义青年团",Charset.forName("GBK")));
-            entity4.addPart("DropDownList4", new StringBody(IdcardUtils.getProvinceByIdCard("320623197512156257"),Charset.forName("GBK")));
-            entity4.addPart("DropDownList5", new StringBody("技校",Charset.forName("GBK")));
-            entity4.addPart("DropDownList2", new StringBody("汉族",Charset.forName("GBK")));
-            entity4.addPart("TextBox3", new StringBody("1234567890"));
-            entity4.addPart("TextBox4", new StringBody(""));
-            entity4.addPart("TextBox5", new StringBody("310000"));
-            entity4.addPart("TextBox26", new StringBody(""));
-            entity4.addPart("TextBox12", new StringBody("1999-07-26"));
-            entity4.addPart("TextBox6", new StringBody("浙江大学",Charset.forName("GBK")));
-            entity4.addPart("TextBox7", new StringBody(""));
-            entity4.addPart("TextBox8", new StringBody("BIYE000012"));
-            entity4.addPart("TextBox25", new StringBody("10001"));
-            entity4.addPart("TextBox13", new StringBody(""));
-            entity4.addPart("TextBox14", new StringBody(""));
-            entity4.addPart("TextBox9", new StringBody(""));
-            entity4.addPart("TextBox15", new StringBody(""));
-            entity4.addPart("TextBox16", new StringBody(""));
-            entity4.addPart("TextBox10", new StringBody(""));
-            entity4.addPart("TextBox17", new StringBody(""));
-            entity4.addPart("TextBox18", new StringBody(""));
-            entity4.addPart("TextBox11", new StringBody(""));
-            entity4.addPart("DropDownList16", new StringBody("自取",Charset.forName("GBK")));
-            entity4.addPart("TextBox20", new StringBody("xxx"));
-            
-            entity4.addPart("DropDownList7", new StringBody("2"));
-            entity4.addPart("DropDownList8", new StringBody("1"));
-            entity4.addPart("DropDownList15", new StringBody("Y"));
-            entity4.addPart("DropDownList6", new StringBody("参加考试",Charset.forName("GBK")));
-            entity4.addPart("DropDownList9", new StringBody("121ZB02Z"));
-            entity4.addPart("DropDownList11", new StringBody(""));
-            entity4.addPart("DropDownList12", new StringBody(""));
-            entity4.addPart("DropDownList10", new StringBody("092"));
-            entity4.addPart("DropDownList13", new StringBody(""));
-            entity4.addPart("DropDownList14", new StringBody(""));
-            entity4.addPart("TextBox21", new StringBody(""));
-            entity4.addPart("TextBox22", new StringBody(""));
-            entity4.addPart("TextBox23", new StringBody(""));
-            entity4.addPart("TextBox24", new StringBody(""));
-            entity4.addPart("Button1", new StringBody("确定添加",Charset.forName("GBK")));
-//            FileBody fileBody = new FileBody(new File(""),"application/octet-stream"); 
-//            entity4.addPart("UploadFile", fileBody);
-            
-            req2.setEntity(entity4);
+            req2.setEntity(new UrlEncodedFormEntity(nvps2, HTTP.UTF_8));
             
             rsp = client.execute(req2);
             entity = rsp.getEntity();
@@ -219,7 +250,7 @@ public class UPOLUploader implements IUploader{
            
             System.out.println(content);
             
-            if(content.contains("请您核对并且打印报名确认表")) 
+            if(content.contains("成功添加")) 
             {
             	return true;
             }
@@ -253,6 +284,14 @@ public class UPOLUploader implements IUploader{
 		return content.substring(content.indexOf("\"__VIEWSTATE\" value=\"") + 21,
         		content.indexOf("\" />",content.indexOf("\"__VIEWSTATE\" value=\"") + 21)
         		) ;
+	}
+	
+	public static void main (String[] argv)
+	{
+		
+		 RecruitAgent agent = new RecruitAgent();
+		 agent.setUserid_for_college("hzzx");
+		 UPOLUploader.getInstance(agent).upload(new StudentInfo());
 	}
 
 }
